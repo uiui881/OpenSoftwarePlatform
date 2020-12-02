c1f0ff6e25d8ed16e6169f1f4fe7ff9ac345d339
jiwooyoo <62748839+jiwooyoo@users.noreply.github.com>
2020-12-02 22:40:34 +0900

    Update ExerciseCheck.php
    DB에 추가되는 상황까지 구현 완료(이후 합치면서 고치면 됨)

--- ExerciseCheck.php
+++ ExerciseCheck.php
@@ -1,31 +1,8 @@
-<!DOCTYPE html> 
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Check</title>
-</head>
-<body>
-   <h1>&nbsp &nbsp Exercise Check</h1>
-
-   <iframe width="560" height="315" src="https://www.youtube.com/embed/OwOfylESbB0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-   </iframe>
-   <frame>
-      <input type="checkbox" name="date1" value="date1" style="background-color: red"><?php echo date("m/d")?></input>
-      <input type="checkbox" id="chkbox"><label for="chkbox"><em></em>체크</label>
-   </frame>
-      <br>
-   
 <?php @ob_start();
    $startdate=new DateTime("11/24");
    $todaydate=new DateTime(date("m/d"));
    
    $dd=date_diff($startdate,$todaydate);
-   
-   for($dayCount=1;$dayCount<=$dd->days+1;$dayCount++){
-      echo "Day ".$dayCount."<br>";
-      echo date_format($startdate,"m/d")."<br>";
-      $startdate->modify('+1 day');
-   }
 
    $conn = mysqli_connect("localhost","root", "159753", "ut");
 
@@ -47,13 +24,60 @@
       
    }
 
-   $sql="SELECT date FROM record";
-   $res = mysqli_query($conn,$sql);
-
-   while ($row = mysqli_fetch_assoc($res)) {
-      printf("date : %s", $row["date"]);
-   }
+  // $sql="SELECT date FROM record";
+  // $res = mysqli_query($conn,$sql);
 
 ?> 
+
+<!DOCTYPE html> 
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Check</title>
+</head>
+<body>
+   <h1>&nbsp &nbsp Exercise Check</h1>
+
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/OwOfylESbB0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+   </iframe>
+   <form action="" method="POST">
+      <label> Day + <?php echo $dd->days?></label><bt>
+      <input type="checkbox" name="chk1"><?php echo date("m/d")?>
+      <input type="submit" name="sub" value="Confirm">
+   </form>
+      <br>
+
+
+<?php
+   $just=$_POST['chk1'];
+   //echo $just;
+
+   if($just=="on"){
+      $sql="insert into record(user_id, video_id, date, done) values (1,1,'".date("Y-m-d")."',TRUE)";
+      $res = mysqli_query($conn,$sql);
+   }
+   
+
+?>
+<!--
+<script type="text/javascript">
+  /* var chk1=document.getElementById('chkBox');
+   var chk_t=chk1.getAttribute("checked");
+   if(chk_t) */
+      function checkClick() {
+        // var chk1=document.getElementById('chkBox');
+         //alert(chk1);
+ 
+         if ($("input:checkbox[name='chk1']").is(":checked") == true) {
+                //체크가 되어있을때.
+               alert("버튼을 클릭하였습니다.");
+
+         } else {
+                //체크가 안되어있을때.
+                alert("실패");
+         }
+      }
+
+</script> -->
 </body>
 </html>
